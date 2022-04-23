b2dfbbca357f5165249a1ae469336d10d5a31eac
Varun Alla <varun.alla@sjsu.edu>
2022-04-23 11:56:00 -0700

    added model for Macroeconomic data

---
+++ app/MEFSApp/app/src/main/java/com/cmpe277/hackathon/mainactivity/models/MacroEconomicDataPoint.java
@@ -0,0 +1,13 @@
+package com.cmpe277.hackathon.mainactivity.models;
+
+public class MacroEconomicDataPoint {
+    String country;
+    int year;
+    //GDP parameters
+    double gdp_growth_rate;
+    double gdp_current_usd;
+    double current_account_balance;
+    double fdi_net;
+    double fdi_net_in;
+    double fdi_net_out;
+}
